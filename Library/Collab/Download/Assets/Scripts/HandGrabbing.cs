8e3fbff6a8cb7bc2f377bb66a254ee8e89439800
Amir <greamdesu@gmail.com>
2019-03-07 14:47:05 +0600

    Initial commit

---
+++ Library/Collab/Download/Assets/Scripts/HandGrabbing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR; //needs to be UnityEngine.VR in Versions before 2017.2
+using System.IO.Ports;
+using System;
+using System.Net.Sockets;
+using System.Net;
+
+public class HandGrabbing : MonoBehaviour
+{
+    float normalizedTime = 0;
+    public Animator anim;
+    int data = 0;
+    GameObject rotater;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        data = Convert.ToInt32(TCPChat.clientMessage);
+        normalizedTime = data/100f;
+        anim.Play("Press", 0, normalizedTime);
+        anim.speed = 0;
+    }
+
+
+
+}
